39d0e2ba22783bff7b0fd9896f4fcf548dba3dac
Miftau <miftaudeenb@gmail.com>
2025-11-16 21:50:26 +0100

    port corrected

--- app.py
+++ app.py
@@ -1807,5 +1807,5 @@ def health():
 
 if __name__ == "__main__":
     # For production use gunicorn (or fly/gunicorn)
-    port = int(os.getenv("PORT", 8080))
+    port = int(os.getenv("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=os.getenv("FLASK_DEBUG", "False") == "True")
